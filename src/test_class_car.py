c8421e52b8e8d76e4b35776d5e486e23b7f93e4e
Vasia0101 <Vasoa0101@gmail.com>
2026-03-17 13:47:24 +0300

    1

--- src/test_class_car.py
+++ src/test_class_car.py
@@ -1,6 +1,3 @@
-import unittest
-from .car import Car
-
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.car = Car(model="BMW X5", fuel_capacity=80)
@@ -9,8 +6,9 @@ class TestCase(unittest.TestCase):
         pass
 
     def test_drive(self):
+        self.car.refuel_car(20)
         self.car.drive(20)
-        self.assertRaises(Exception, lambda: self.car.drive(80000))
+        self.assertTrue(self.car.get_current_fuel_level() < 20)
 
     def test_refuel(self):
         # Заправим 20 литров
